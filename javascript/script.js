4096f67a0d9c51cd7f872c7b1e2fd39f77079a87
boris2442 <aubinborissimotsebo@gmail.com>
2024-12-02 10:18:41 +0100

    introduction au javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,24 @@
-const header=document.getElementById("main-header");
+// Sélectionne le header
+const header = document.getElementById("main-header");
+
+// Événement scroll
 window.addEventListener("scroll", () => {
-if(window.scrollY>50){
-    header.style.background="red";
-    
+  if (window.scrollY > 520) {
+    // Ajout dynamique de styles via JavaScript
+    header.style.backgroundColor = "var(--color2)"; // Change le fond du header
+    header.style.transition="1s ease-in-out";
+    const links = header.querySelectorAll("a");
+    links.forEach(link => {
+      link.style.color = "var(--color3)"; // Change la couleur des liens
+    });
+  } else {
+    // Réinitialisation des styles
+    header.style.backgroundColor = "var(--color3)";
+    const links = header.querySelectorAll("a");
+    links.forEach(link => {
+      link.style.color = "var(--color4)"; // Rétablit la couleur des liens
+    });
+  }
+});
 
-}
 
-
-
-
-}
