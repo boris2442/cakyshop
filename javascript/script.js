380973e56ff3b5baa38c0cd317761bead571cc93
boris2442 <aubinborissimotsebo@gmail.com>
2024-12-25 06:12:16 +0100

    javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,57 +1,56 @@
 // Sélectionne le header
-const header = document.getElementById("main-header");
 
-// Événement scroll
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 520) {
-    // Ajout dynamique de styles via JavaScript
-    header.style.backgroundColor = "var(--color2)"; // Change le fond du header
-    header.style.transition="1s ease-in-out";
-    const links = header.querySelectorAll("a");
-    links.forEach(link => {
-      link.style.color = "var(--color4)"; 
-    });
-  } else {
-    // Réinitialisation des styles
-    header.style.backgroundColor = "var(--color3)";
-    const links = header.querySelectorAll("a");
-    links.forEach(link => {
-      link.style.color = "var(--color2)"; // Rétablit la couleur des liens
-      link.style.fontWeight="bold";
-    });
-  }
-});
+// const header = document.getElementById("main-header");
 
-// Sélection du header et du h2
-const header2 = document.getElementById("main-header2");
-const headerText = header.querySelector("h2");
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 520) {
+  
+//     header.style.backgroundColor = "var(--color2)"; 
+//     header.style.transition="1s ease-in-out";
+//     links.forEach(link => {
+//       link.style.color = "var(--color4)"; 
+//     });
+//   } else {
+ 
+//     header.style.backgroundColor = "var(--color3)";
+//     const links = header.querySelectorAll("a");
+//     links.forEach(link => {
+//       link.style.color = "var(--color2)"; 
+//       link.style.fontWeight="bold";
+//     });
+//   }
+// });
 
-// Ajout de l'événement scroll
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    // Change la couleur du texte de h2 en blanc
-    headerText.style.color = "black";
-    header.style.backgroundColor = "red"; // Facultatif : Change le fond du header
-  } else {
-    // Réinitialise la couleur du texte
-    headerText.style.color = "black";
-    header2.style.backgroundColor = "transparent"; // Facultatif : Réinitialise le fond
-  }
-});
+
+// const header2 = document.getElementById("main-header2");
+// const headerText = header.querySelector("h2");
+
+
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 50) {
+
+//     headerText.style.color = "black";
+//     header.style.backgroundColor = "red"; 
+//   } else {
+   
+//     headerText.style.color = "black";
+//     header2.style.backgroundColor = "transparent"; 
+//   }
+// });
 
 
 ///////////////////////////////////////////////////////autre code javascript
 
-const active= document.querySelector("header>ul");
-const burgerMenu=document.querySelector(".burger");
-burgerMenu.addEventListener("click", ()=>{
-if(window.scrollX<815){
-  active.classList.toggle("active")
+// const active= document.querySelector("header>ul");
+// const burgerMenu=document.querySelector(".burger");
+// burgerMenu.addEventListener("click", ()=>{
+// if(window.scrollX<815){
+//   active.classList.toggle("active")
 
-}else{
-  active.classList.add("active2")
-  active.classList.toggle("active")
-}
+// }else{
+//   active.classList.add("active2")
+//   active.classList.toggle("active")
+// }
 
 
   // if(window.scrollY>200){
@@ -60,8 +59,9 @@ if(window.scrollX<815){
   //     link.style.fontWeight="bold";
   //   });
   // }
-})
+// })
 
+//####################### librairie typewriter########################333
 
 const secondTitle=document.querySelector("#accueil h2");
 const typewriter= new Typewriter (secondTitle, {
@@ -91,7 +91,7 @@ app
 
 
 
-// partie destinee au swiper library 
+//############################# partie destinee au swiper library 
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
